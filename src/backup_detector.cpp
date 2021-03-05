a3ba8754878187523a70d2fa2a6f1e7a35768d85
mohradza <mioh0887@colorado.edu>
2021-03-04 23:02:36 -0700

    working backup detector node

--- src/backup_detector.cpp
+++ src/backup_detector.cpp
@@ -10,7 +10,6 @@ backupDetector::backupDetector(const ros::NodeHandle &node_handle,
 
 void backupDetector::init() {
 
-  sub_laserscan_ = nh_.subscribe("laserscan", 1, &backupDetector::laserscanCb, this);
   sub_octomap_ = nh_.subscribe("octomap", 1, &backupDetector::octomapCb, this);
   sub_imu_ = nh_.subscribe("imu", 1, &backupDetector::imuCb, this);
 
@@ -18,9 +17,8 @@ void backupDetector::init() {
   pub_query_point_pcl_ = nh_.advertise<sensor_msgs::PointCloud2>("query_points", 1);
   pub_transformed_query_point_pcl_ = nh_.advertise<sensor_msgs::PointCloud2>("transformed_query_points", 1);
   pub_occupied_points_pcl_ = nh_.advertise<sensor_msgs::PointCloud2>("occupied_points", 1);
+  pub_close_points_pcl_ = nh_.advertise<sensor_msgs::PointCloud2>("close_points", 1);
 
-  pnh_.param("total_scan_points", num_scan_points_, 100);
-  pnh_.param("min_turnaround_distance", min_turnaround_distance_, 0.1);
   pnh_.param("pitch_limit", pitch_limit_, 20.0);
 
   // Query point grid parameters
@@ -34,31 +32,22 @@ void backupDetector::init() {
   pnh_.param("resolution", resolution_, 0.15);
 
   pnh_.param<std::string>("vehicle_name", vehicle_name_,"X1");
-  pnh_.param<std::string>("world_frame", world_frame_, "world");
-  pnh_.param<std::string>("base_link_frame", base_link_frame_, "base_link");
-  world_frame_ = "world";
-  base_link_frame_ = "X1/base_link";
+  pnh_.param<std::string>("map_frame", map_frame_, "world");
 
-
+  base_link_frame_ = vehicle_name_ + "/base_link";
 
   backup_msg_.data = false;
   enable_debug_ = true;
   close_obstacle_flag_ = false;
   bad_attitude_flag_ = false;
-  have_scan_ = false;
   have_imu_ = false;
   have_octomap_ = false;
-  num_query_point_rows_ = 10;
-  num_query_point_cols_ = 10;
-  num_query_point_layers_ = 10;
+
   num_query_points_ = num_query_point_rows_*num_query_point_cols_*num_query_point_layers_;
   query_point_spacing_ = resolution_;
 
   // Merged OcTree
   occupancyTree_ = new octomap::OcTree(resolution_);
-
-  // Initialize the laserscan vector
-  laserscan_ranges_.resize(num_scan_points_);
 
   // Generate the query points for octomap voxel lookups
   generateQueryPoints();
@@ -106,18 +95,11 @@ void backupDetector::publishQueryPointsPcl(){
   }
 
   pcl::toROSMsg(transformed_query_point_pcl, cloud_msg);
-  cloud_msg.header.frame_id = world_frame_;
+  cloud_msg.header.frame_id = map_frame_;
   cloud_msg.header.stamp = ros::Time::now();
 
   pub_transformed_query_point_pcl_.publish(cloud_msg);
 
-}
-
-void backupDetector::laserscanCb(const sensor_msgs::LaserScanConstPtr& scan_msg)
-{
-  if(!have_scan_) have_scan_ = true;
-  // Import the laserscan
-  laserscan_ranges_ = scan_msg->ranges;
 }
 
 void backupDetector::octomapCb(const octomap_msgs::Octomap::ConstPtr msg)
@@ -139,20 +121,6 @@ void backupDetector::imuCb(const sensor_msgs::ImuConstPtr& imu_msg){
     //float temp_roll = roll_;
     //roll_ = pitch_;
     //pitch_ = -temp_roll;
-}
-
-void backupDetector::processLaserscan(){
-  // Check the octomap laserscan to see if we can back up
-  // If the minimum value of the scan is less than the vehicle length (min_turnaround_distance_)
-  // then the vehicle needs to back up instead of turn around.
-  float min_distance = *min_element(laserscan_ranges_.begin(), laserscan_ranges_.end() );
-
-  if (min_distance < min_turnaround_distance_){
-    close_obstacle_flag_ = true;
-  } else {
-    close_obstacle_flag_ = false;
-  }
-
 }
 
 void backupDetector::transformQueryPoints(const geometry_msgs::TransformStamped transform_stamped){
@@ -188,28 +156,26 @@ void backupDetector::processOctomap(){
   }
 
   // Check each keyed voxel for occupancy
+  // Check all occupied cells to see if they exist
+  // within some predefined safety radius around the vehicle
+  // Also have a height check for tuning voxels near the wheels
   occupied_cell_indices_vec_.clear();
-  //vector<int> occupied_cell_indices_vec_;
+  close_cell_indices_vec_.clear();
+  float voxel_radius;
+  close_obstacle_flag_ = false;
+
   for(int i = 0; i < num_query_points_; i++){
     octomap::OcTreeNode* current_node = occupancyTree_->search(coord_key_vec_[i]);
     if(current_node && current_node->getLogOdds() >= 0.0){
       // Cell is occupied, need to track the index
       occupied_cell_indices_vec_.push_back(i);
-    }
-  }
-
-  // Check all occupied cells to see if they exist
-  // within some predefined safety radius around the vehicle
-  // Also have a height check for tuning voxels near the wheels
-  float voxel_radius;
-  close_obstacle_flag_ = false;
-  int index;
-  for (int i = 0; i < occupied_cell_indices_vec_.size(); i++){
-    index = occupied_cell_indices_vec_[i];
-    voxel_radius = sqrt(pow(query_point_vec_[index].x, 2) + pow(query_point_vec_[index].y, 2));
-    if(voxel_radius < safety_radius_){
-      if(query_point_vec_[index].z > z_lower_threshold_){
-       close_obstacle_flag_ = true;
+      // Check if the occupied cell is within our safety cylinder
+      voxel_radius = sqrt(pow(query_point_vec_[i].x, 2) + pow(query_point_vec_[i].y, 2));
+      if(voxel_radius < safety_radius_ && query_point_vec_[i].z > z_lower_threshold_){
+        if(query_point_vec_[i].z > z_lower_threshold_){
+         close_obstacle_flag_ = true;
+         close_cell_indices_vec_.push_back(i);
+        }
       }
     }
   }
@@ -217,6 +183,7 @@ void backupDetector::processOctomap(){
   if (enable_debug_){
     pcl::PointCloud<pcl::PointXYZ> point_pcl;
     pcl::PointXYZ pcl_point;
+    int index;
     for (int i = 0; i < occupied_cell_indices_vec_.size(); i++){
       index = occupied_cell_indices_vec_[i];
       pcl_point = {transformed_query_point_vec_[index].point.x, transformed_query_point_vec_[index].point.y, transformed_query_point_vec_[index].point.z};
@@ -225,10 +192,23 @@ void backupDetector::processOctomap(){
 
     sensor_msgs::PointCloud2 cloud_msg;
     pcl::toROSMsg(point_pcl, cloud_msg);
-    cloud_msg.header.frame_id = world_frame_;
+    cloud_msg.header.frame_id = map_frame_;
     cloud_msg.header.stamp = ros::Time::now();
 
     pub_occupied_points_pcl_.publish(cloud_msg);
+
+    point_pcl.clear();
+    for (int i = 0; i < close_cell_indices_vec_.size(); i++){
+      index = close_cell_indices_vec_[i];
+      pcl_point = {transformed_query_point_vec_[index].point.x, transformed_query_point_vec_[index].point.y, transformed_query_point_vec_[index].point.z};
+      point_pcl.push_back(pcl_point);
+    }
+
+    pcl::toROSMsg(point_pcl, cloud_msg);
+    cloud_msg.header.frame_id = map_frame_;
+    cloud_msg.header.stamp = ros::Time::now();
+
+    pub_close_points_pcl_.publish(cloud_msg);
   }
 
 }
@@ -237,6 +217,7 @@ void backupDetector::processIMU(){
   // Check to see if the vehicle is pitched past the limit.
   // If so, we need to backup instead of turning around to prevent
   // the vehicle from slipping sideways in sim.
+  ROS_INFO_THROTTLE(.5,"pitch: %f", pitch_);
   if(abs(pitch_) > pitch_limit_){
     bad_attitude_flag_ = true;
   } else {
@@ -253,12 +234,15 @@ bool backupDetector::haveIMU(){
   return have_imu_;
 }
 
-bool backupDetector::haveScan(){
-  return have_scan_;
-}
-
 bool backupDetector::haveOctomap(){
   return have_octomap_;
+}
+
+vector<string> backupDetector::getFrames(){
+  vector<string> frames;
+  frames.push_back(map_frame_);
+  frames.push_back(base_link_frame_);
+  return frames;
 }
 
 
--- src/backup_detector_node.cpp
+++ src/backup_detector_node.cpp
@@ -9,6 +9,7 @@ int main(int argc, char** argv) {
     backup_detector::backupDetector backup_detector(nh, nh_private);
 
     backup_detector.generateQueryPoints();
+    vector<string> frames = backup_detector.getFrames();
 
     // Transform buffer
     tf2_ros::Buffer tf_buffer;
@@ -18,33 +19,29 @@ int main(int argc, char** argv) {
     ros::Rate loop_rate(10);
 
     while(ros::ok()){
-        // Check for a new tf
 
+        // Check for a new tf
         try {
-          transform_stamped = tf_buffer.lookupTransform("world", "X1/base_link", ros::Time(0));
+          transform_stamped = tf_buffer.lookupTransform(frames[0], frames[1], ros::Time(0));
         } catch (tf2::TransformException &ex) {
           ROS_WARN("%s",ex.what());
           ros::Duration(1.0).sleep();
           continue;
         }
 
-        ROS_INFO_THROTTLE(1.0,"transform x: %f", transform_stamped.transform.translation.x);
-        // if(backup_detector.haveScan()){
-        //   backup_detector.processLaserscan();
-        // }
-        //
-        // if(backup_detector.haveIMU()){
-        //   backup_detector.processIMU();
-        // }
-        //
+        // Check if our attitude is outside of the safety bounds
+        if(backup_detector.haveIMU()){
+          backup_detector.processIMU();
+        }
 
+        // Check if we are clear of obstacles
         if(backup_detector.haveOctomap()){
-          ROS_INFO_THROTTLE(1.0,"Processing octomap...");
           backup_detector.transformQueryPoints(transform_stamped);
           backup_detector.processOctomap();
         }
 
-        // backup_detector.publishBackupMsg();
+        // Publish results
+        backup_detector.publishBackupMsg();
         backup_detector.publishQueryPointsPcl();
         ros::spinOnce();
         loop_rate.sleep();
